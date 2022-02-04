405834dbd38e9afa093962118d9a330f18ce8910
alaudeenmusthafa <98819975+alaudeenmusthafa@users.noreply.github.com>
2022-02-04 16:19:11 +0530

    Master updates for FeatureBranch2

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -7,8 +7,9 @@ public class LoginTest {
 	@Test
 	public void test1() {
 
-		System.out.println("This is test1");
-		System.out.println("This is second line");
+		System.out.println("This is test2");
+		System.out.println("This is second");
+		System.out.println("This is third line");
 		
 	}
 }
